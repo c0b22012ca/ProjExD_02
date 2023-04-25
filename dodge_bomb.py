8821a1524460ccde260c2f8d4b297c09beafea1b
izumi <c0b22012ca@edu.teu.ac.jp>
2023-04-25 14:50:49 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,11 @@ import sys
 
 import pygame as pg
 
+de = {pg.K_UP:((0,-1)),#移動の辞書
+          pg.K_DOWN:((0,+1)),
+          pg.K_LEFT:((-1,0)),
+          pg.K_RIGHT:((+1,0))}
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -12,6 +17,10 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     tmr = 0
+
+    kk_rct = kk_img.get_rect()#rectクラスの生成と値ゲット
+    kk_rct.center = 900,400
+
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     bb_img.set_colorkey((0,0,0))
@@ -20,17 +29,23 @@ def main():
     vx = +1
     vy = +1
     bb_rect = bb_img.get_rect()#rectクラスの生成と値ゲット
-    bb_rect.center = bb_x,bb_y
+    bb_rect.center = bb_x,bb_y#rectクラスのcenterインスタンスの変更
     bb_rect.move_ip(vx,vy)
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
 
         tmr += 1
+        key_lst = pg.key.get_pressed()
+        for k,v in de.items():#kが押されたキー、ｖが移動方向
+            if key_lst[k]:
+                kk_rct.move_ip(v)
+
         bb_rect.move_ip(vx,vy)
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         screen.blit(bb_img,bb_rect)
         pg.display.update()
         clock.tick(1000)
