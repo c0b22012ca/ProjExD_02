c132f9d5623e1067d99f5ee321fe12d920c6667d
izumi <c0b22012ca@edu.teu.ac.jp>
2023-04-25 14:34:48 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,17 +15,23 @@ def main():
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     bb_img.set_colorkey((0,0,0))
-    bb_x=random.randint(0,1600)
-    bb_y=random.randint(0,900)
+    bb_x = random.randint(0,1600)
+    bb_y = random.randint(0,900)
+    vx = +1
+    vy = +1
+    bb_rect = bb_img.get_rect()#rectクラスの生成と値ゲット
+    bb_rect.center = bb_x,bb_y
+    bb_rect.move_ip(vx,vy)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
 
         tmr += 1
+        bb_rect.move_ip(vx,vy)
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img,[bb_x,bb_y])
+        screen.blit(bb_img,bb_rect)
         pg.display.update()
         clock.tick(1000)
 
