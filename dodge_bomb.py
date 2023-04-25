52bdc5d82e6b28f5b7a7fe439cb79b5ab80b74b3
izumi <c0b22012ca@edu.teu.ac.jp>
2023-04-25 15:46:11 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,10 +4,27 @@ import sys
 
 import pygame as pg
 
+
 de = {pg.K_UP:((0,-1)),#移動の辞書
           pg.K_DOWN:((0,+1)),
           pg.K_LEFT:((-1,0)),
           pg.K_RIGHT:((+1,0))}
+
+
+def check_bound(screen_rect: pg.rect,obj_rect:  pg.rect) -> tuple[bool,bool]: 
+    """
+    オブジェクトが画面内か画面外かを判定
+    引数１ 画面surfaceのRect
+    引数２ オブジェクトSurfaceのRect
+    戻り値 画面内:True 画面外:False
+    """
+    yoko, tate = True, True
+    if obj_rect.left < screen_rect.left or screen_rect.right < obj_rect.right:
+        yoko = False
+    if obj_rect.top < screen_rect.top or screen_rect.bottom < obj_rect.bottom:
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -19,18 +36,18 @@ def main():
     tmr = 0
 
     kk_rct = kk_img.get_rect()#rectクラスの生成と値ゲット
-    kk_rct.center = 900,400
+    kk_rct.center = 900,400#コウカトンの最初の位置
 
-    bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_img.set_colorkey((0,0,0))
+    bb_img = pg.Surface((20,20))#爆弾の書かれているスクリーン
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)#爆弾のスクリーンに爆弾を書く
+    bb_img.set_colorkey((0,0,0))#爆弾スクリーンの透過
     bb_x = random.randint(0,1600)
     bb_y = random.randint(0,900)
     vx = +1
     vy = +1
     bb_rect = bb_img.get_rect()#rectクラスの生成と値ゲット
     bb_rect.center = bb_x,bb_y#rectクラスのcenterインスタンスの変更
-    bb_rect.move_ip(vx,vy)
+
     
     while True:
         for event in pg.event.get():
@@ -38,12 +55,22 @@ def main():
                 return 0
 
         tmr += 1
+
         key_lst = pg.key.get_pressed()
         for k,v in de.items():#kが押されたキー、ｖが移動方向
             if key_lst[k]:
-                kk_rct.move_ip(v)
+                kk_rct.move_ip(v)#移動
 
-        bb_rect.move_ip(vx,vy)
+        if check_bound(screen.get_rect(),kk_rct) != (True,True):
+             for k,v in de.items():#kが押されたキー、ｖが移動方向
+                if key_lst[k]:
+                    kk_rct.move_ip(-v[0], -v[1])#移動
+        bb_rect.move_ip(vx,vy)#移動の
+        yoko , tate = check_bound(screen.get_rect(),bb_rect)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img,bb_rect)
