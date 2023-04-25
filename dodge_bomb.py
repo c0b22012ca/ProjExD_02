a4c952ff85bf8842b08101bdea1d109f1b6b9ecb
izumi <c0b22012ca@edu.teu.ac.jp>
2023-04-25 14:11:08 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,11 +5,13 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
-    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex02/fig/3.png")
+    bg_img = pg.image.load("fig/pg_bg.jpg")#
+    kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     tmr = 0
-
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -18,7 +20,6 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-
         pg.display.update()
         clock.tick(1000)
 
